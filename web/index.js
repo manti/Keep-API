25c139a25d7e0eb178ece10003a1b8f13bd5a689
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-10-31 04:27:00 +0530

    some boilerplate

--- web/index.js
+++ web/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const keep = require('keep')
+// const { sendConfirmation } = require('./email')
 
 const app = express()
 
@@ -11,7 +12,10 @@ app.use('/static', express.static('static'))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
 app.post('/register', (req, res) => {
   const { email } = req.body
-  keep.set('users', { email }).then(_ => res.end())
+  keep.set('users', { email }).then(_ => {
+    // sendConfirmation(email)
+    res.end()
+  })
 })
 
 app.listen(5000, () => console.log('server ready'))
