d1d007f4ee5a6f62440b3f0a0c200f8d7b0a7aaa
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-10-31 04:13:30 +0530

    register

--- web/index.js
+++ web/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
+const keep = require('keep')
 
 const app = express()
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/static', express.static('static'))
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
+app.post('/register', (req, res) => {
+  const { email } = req.body
+  keep.set('users', { email }).then(_ => res.end())
+})
 
 app.listen(5000, () => console.log('server ready'))
