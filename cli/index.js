0c74a30971e55d9003eda1f16fdf2df1fa931eb2
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-12-06 09:51:28 +0100

    write secrets to file

--- cli/index.js
+++ cli/index.js
@@ -1,11 +1,23 @@
 const axios = require('axios')
 const prompt = require('prompt')
+const fs = require('fs')
 
 const url = 'http://localhost:3000/init'
 
 prompt.get('email', function(err, { email }) {
   axios
     .post(url, { email })
-    .then(response => console.log(response.data))
+    .then(response => append(response.data))
     .catch(error => console.log(error.message))
 })
+
+const append = ({ appId, secret }) => {
+  const exists = fs.existsSync('.env')
+  let content = ''
+  if (exists) content = fs.readFileSync('.env')
+  content += `
+KEEP_APP_ID=${appId}
+KEEP_SECRET=${secret}
+`
+  fs.writeFileSync('.env', content)
+}
